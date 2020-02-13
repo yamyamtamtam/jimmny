f8f0bef704668d759f3b80da9e5c731961758283
unknown <7623@suda.co.jp>
2020-02-13 19:02:56 +0900

    20200213

--- doseido/header.php
+++ doseido/header.php
@@ -12,6 +12,9 @@
 </head>
 <body <?php body_class(); ?>>
 <header class="header">
+  <div class="header-deco">
+    <div class="header-deco__center"></div>
+  </div>
   <section class="header-top">
     <h1 class="header-logo"><a href="<?php echo esc_url(home_url( '/' )); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-swing.png" alt="<?php bloginfo('name'); ?>"></a></h1>
     <p class="header-description"><?php bloginfo('description'); ?></p>
